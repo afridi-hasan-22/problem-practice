64a6dfdfccf77ed12ea962023a503d6aeac95405
Afridi <afridi.dcc6251@gmail.com>
2023-02-06 18:41:03 +0600

    added

---
+++ case.js
@@ -0,0 +1,9 @@
+const userName = 'blackPink';
+const userInput = 'blackpinK';
+
+if(userName.toLowerCase === userInput.toLowerCase ){
+    console.log('user valid');
+}
+else{
+  console.log('user invalid');
+}
---
+++ search.js
@@ -0,0 +1,4 @@
+const names = ('Afridi Hasan Mahir');
+const doesexist = names.includes('Hasan');
+console.log(doesexist);
+console.log(names.indexOf('Mahir'));
